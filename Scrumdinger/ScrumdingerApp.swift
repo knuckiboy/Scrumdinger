182771fd04bbb2672f8a7d584f83947ecde96da2
Jian Nan Ng <jiannan.ng@zuhlke.com>
2022-10-14 16:26:21 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jian Nan Ng on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
